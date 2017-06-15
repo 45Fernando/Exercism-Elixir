6cb2deb0d9fe6fb9db8738f11ef2cda99bf220ce
Fernando <fernandoexequielorquera@gmail.com>
2017-06-15 11:48:07 -0300

    Add files via upload

---
+++ largest-series-product/largest_series_product.exs
@@ -0,0 +1,24 @@
+defmodule Series do
+  @doc """
+  Finds the largest product of a given number of consecutive numbers in a given string of numbers.
+  """
+  @spec largest_product(String.t, non_neg_integer) :: non_neg_integer
+
+  def largest_product(_number_string, 0), do: 1
+  def largest_product(number_string, size) when byte_size(number_string) < size or size < 0 do
+    raise ArgumentError
+  end
+
+  def largest_product(number_string, size) do
+    number_string
+    |> String.graphemes
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk(size, 1)
+    |> Stream.map(&prod/1)
+    |> Enum.max
+  end
+
+  defp prod([a]), do: a
+  defp prod([a | tail]), do: a * prod(tail)
+
+end
